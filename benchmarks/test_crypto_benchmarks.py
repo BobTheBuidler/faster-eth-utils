75c15f7e4890cb72e4c0aa25b057443c16ce429e
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2025-09-06 18:51:49 +0000

    feat(benchmark): parametrize crypto benchmarks

--- benchmarks/test_crypto_benchmarks.py
+++ benchmarks/test_crypto_benchmarks.py
@@ -1,3 +1,4 @@
+# mypy: disable-error-code=misc
 from typing import Any, Callable
 
 import eth_utils
@@ -6,19 +7,26 @@ from pytest_codspeed import BenchmarkFixture
 
 import faster_eth_utils
 
-
 def _batch(i: int, fn: Callable[..., Any], *inputs: Any) -> None:
     for _ in range(i):
         fn(*inputs)
 
+keccak_cases = [
+    b"hello world",         # bytes
+    123456789,              # int
+    True,                   # bool
+    "0x68656c6c6f",         # hexstr
+    "hello world",          # text
+    None,                   # invalid (should raise)
+    [1, 2, 3],              # invalid (should raise)
+]
 
 @pytest.mark.benchmark(group="keccak")
-def test_keccak(benchmark: BenchmarkFixture) -> None:
-    data = b"hello world"
-    benchmark(_batch, 100, eth_utils.keccak, data)
-
+@pytest.mark.parametrize("value", keccak_cases)
+def test_keccak(benchmark: BenchmarkFixture, value: Any) -> None:
+    benchmark(_batch, 10, eth_utils.keccak, value)
 
 @pytest.mark.benchmark(group="keccak")
-def test_faster_keccak(benchmark: BenchmarkFixture) -> None:
-    data = b"hello world"
-    benchmark(_batch, 100, faster_eth_utils.keccak, data)
+@pytest.mark.parametrize("value", keccak_cases)
+def test_faster_keccak(benchmark: BenchmarkFixture, value: Any) -> None:
+    benchmark(_batch, 10, faster_eth_utils.keccak, value)
