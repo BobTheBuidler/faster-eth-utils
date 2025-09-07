53351bce73f9e03f0f2670c320f3aca8ca85aa17
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-09-07 01:11:09 -0400

    fix(benchmark): fix codspeed.io processing

--- benchmarks/test_conversions_benchmarks.py
+++ benchmarks/test_conversions_benchmarks.py
@@ -12,12 +12,12 @@ def _batch(i: int, fn: Callable[..., Any], *args: Any, **kwargs: Any) -> None:
     for _ in range(i):
         fn(*args, **kwargs)
 
-to_hex_kwargs_cases: List[Dict[str, Any]] = [
-    {"primitive": b"helloworld"},
-    {"primitive": 123456789},
-    {"primitive": True},
-    {"hexstr": "0x68656c6c6f"},
-    {"text": "hello world"},
+to_hex_kwargs_cases: List[Tuple[str, Any]] = [
+    ("primitive", b"helloworld"),
+    ("primitive", 123456789),
+    ("primitive", True),
+    ("hexstr", "0x68656c6c6f"),
+    ("text", "hello world"),
 ]
 to_hex_ids = [
     "primitive-bytes",
@@ -27,12 +27,12 @@ to_hex_ids = [
     "text",
 ]
 
-to_int_kwargs_cases: List[Dict[str, Any]] = [
-    {"primitive": 123456789},
-    {"primitive": True},
-    {"primitive": b"\x07[\xcd\x15"},
-    {"hexstr": "0x68656c6c6f"},
-    {"text": "12345"},
+to_int_kwargs_cases: List[Tuple[str, Any]] = [
+    ("primitive", 123456789),
+    ("primitive", True),
+    ("primitive", b"\x07[\xcd\x15"),
+    ("hexstr", "0x68656c6c6f"),
+    ("text", "12345"),
 ]
 to_int_ids = [
     "primitive-int",
@@ -42,12 +42,12 @@ to_int_ids = [
     "text",
 ]
 
-to_bytes_kwargs_cases: List[Dict[str, Any]] = [
-    {"primitive": 123456789},
-    {"primitive": b"helloworld"},
-    {"primitive": True},
-    {"hexstr": "0x68656c6c6f"},
-    {"text": "hello world"},
+to_bytes_kwargs_cases: List[Tuple[str, Any]] = [
+    ("primitive", 123456789),
+    ("primitive", b"helloworld"),
+    ("primitive", True),
+    ("hexstr", "0x68656c6c6f"),
+    ("text", "hello world"),
 ]
 to_bytes_ids = [
     "primitive-int",
@@ -57,10 +57,10 @@ to_bytes_ids = [
     "text",
 ]
 
-to_text_kwargs_cases: List[Dict[str, Any]] = [
-    {"primitive": b"helloworld"},
-    {"hexstr": "0x68656c6c6f"},
-    {"text": "hello world"},
+to_text_kwargs_cases: List[Tuple[str, Any]] = [
+    ("primitive", b"helloworld"),
+    ("hexstr", "0x68656c6c6f"),
+    ("text", "hello world"),
 ]
 to_text_ids = [
     "primitive-bytes",
@@ -68,19 +68,19 @@ to_text_ids = [
     "text",
 ]
 
-text_if_str_cases: List[Tuple[Any, ...]] = [
-    ("helloworld",),  # str
-    (b"helloworld",), # bytes
+text_if_str_cases: List[Any] = [
+    "helloworld",  # str
+    b"helloworld", # bytes
 ]
 text_if_str_ids = [
     "str",
     "bytes",
 ]
 
-hexstr_if_str_cases: List[Tuple[Any, ...]] = [
-    ("0x68656c6c6f",),  # hexstr
-    (b"helloworld",),   # bytes
-    (123456789,),       # int
+hexstr_if_str_cases: List[Any]] = [
+    "0x68656c6c6f",  # hexstr
+    b"helloworld",   # bytes
+    123456789,       # int
 ]
 hexstr_if_str_ids = [
     "hexstr",
@@ -89,61 +89,61 @@ hexstr_if_str_ids = [
 ]
 
 @pytest.mark.benchmark(group="to_hex")
-@pytest.mark.parametrize("kwargs", to_hex_kwargs_cases, ids=to_hex_ids)
-def test_to_hex(benchmark: BenchmarkFixture, kwargs: Dict[str, Any]) -> None:
-    benchmark(_batch, 10, eth_utils.to_hex, **kwargs)
+@pytest.mark.parametrize("kwarg,value", to_hex_kwargs_cases, ids=to_hex_ids)
+def test_to_hex(benchmark: BenchmarkFixture, kwarg: str, value: Any)) -> None:
+    benchmark(_batch, 10, eth_utils.to_hex, **{kwarg: value})
 
 @pytest.mark.benchmark(group="to_hex")
-@pytest.mark.parametrize("kwargs", to_hex_kwargs_cases, ids=to_hex_ids)
-def test_faster_to_hex(benchmark: BenchmarkFixture, kwargs: Dict[str, Any]) -> None:
-    benchmark(_batch, 10, faster_eth_utils.to_hex, **kwargs)
+@pytest.mark.parametrize("kwarg,value", to_hex_kwargs_cases, ids=to_hex_ids)
+def test_faster_to_hex(benchmark: BenchmarkFixture, kwarg: str, value: Any)) -> None:
+    benchmark(_batch, 10, faster_eth_utils.to_hex, **{kwarg: value})
 
 @pytest.mark.benchmark(group="to_int")
-@pytest.mark.parametrize("kwargs", to_int_kwargs_cases, ids=to_int_ids)
-def test_to_int(benchmark: BenchmarkFixture, kwargs: Dict[str, Any]) -> None:
-    benchmark(_batch, 10, eth_utils.to_int, **kwargs)
+@pytest.mark.parametrize("kwarg,value", to_int_kwargs_cases, ids=to_int_ids)
+def test_to_int(benchmark: BenchmarkFixture, kwarg: str, value: Any) -> None:
+    benchmark(_batch, 10, eth_utils.to_int, **{kwarg: value})
 
 @pytest.mark.benchmark(group="to_int")
-@pytest.mark.parametrize("kwargs", to_int_kwargs_cases, ids=to_int_ids)
-def test_faster_to_int(benchmark: BenchmarkFixture, kwargs: Dict[str, Any]) -> None:
-    benchmark(_batch, 10, faster_eth_utils.to_int, **kwargs)
+@pytest.mark.parametrize("kwarg,value", to_int_kwargs_cases, ids=to_int_ids)
+def test_faster_to_int(benchmark: BenchmarkFixture, kwarg: str, value: Any) -> None:
+    benchmark(_batch, 10, faster_eth_utils.to_int, **{kwarg: value})
 
 @pytest.mark.benchmark(group="to_bytes")
-@pytest.mark.parametrize("kwargs", to_bytes_kwargs_cases, ids=to_bytes_ids)
-def test_to_bytes(benchmark: BenchmarkFixture, kwargs: Dict[str, Any]) -> None:
-    benchmark(_batch, 10, eth_utils.to_bytes, **kwargs)
+@pytest.mark.parametrize("kwarg,value", to_bytes_kwargs_cases, ids=to_bytes_ids)
+def test_to_bytes(benchmark: BenchmarkFixture, kwarg: str, value: Any) -> None:
+    benchmark(_batch, 10, eth_utils.to_bytes, **{kwarg: value})
 
 @pytest.mark.benchmark(group="to_bytes")
-@pytest.mark.parametrize("kwargs", to_bytes_kwargs_cases, ids=to_bytes_ids)
-def test_faster_to_bytes(benchmark: BenchmarkFixture, kwargs: Dict[str, Any]) -> None:
-    benchmark(_batch, 10, faster_eth_utils.to_bytes, **kwargs)
+@pytest.mark.parametrize("kwarg,value", to_bytes_kwargs_cases, ids=to_bytes_ids)
+def test_faster_to_bytes(benchmark: BenchmarkFixture, kwarg: str, value: Any) -> None:
+    benchmark(_batch, 10, faster_eth_utils.to_bytes, **{kwarg: value})
 
 @pytest.mark.benchmark(group="to_text")
-@pytest.mark.parametrize("kwargs", to_text_kwargs_cases, ids=to_text_ids)
-def test_to_text(benchmark: BenchmarkFixture, kwargs: Dict[str, Any]) -> None:
-    benchmark(_batch, 10, eth_utils.to_text, **kwargs)
+@pytest.mark.parametrize("kwarg,value", to_text_kwargs_cases, ids=to_text_ids)
+def test_to_text(benchmark: BenchmarkFixture, kwarg: str, value: Any) -> None:
+    benchmark(_batch, 10, eth_utils.to_text, **{kwarg: value})
 
 @pytest.mark.benchmark(group="to_text")
-@pytest.mark.parametrize("kwargs", to_text_kwargs_cases, ids=to_text_ids)
-def test_faster_to_text(benchmark: BenchmarkFixture, kwargs: Dict[str, Any]) -> None:
-    benchmark(_batch, 10, faster_eth_utils.to_text, **kwargs)
+@pytest.mark.parametrize("kwarg,value", to_text_kwargs_cases, ids=to_text_ids)
+def test_faster_to_text(benchmark: BenchmarkFixture, kwarg: str, value: Any) -> None:
+    benchmark(_batch, 10, faster_eth_utils.to_text, **{kwarg: value})
 
 @pytest.mark.benchmark(group="text_if_str")
-@pytest.mark.parametrize("args", text_if_str_cases, ids=text_if_str_ids)
-def test_text_if_str(benchmark: BenchmarkFixture, args: Tuple[Any, ...]) -> None:
-    benchmark(_batch, 10, eth_utils.text_if_str, eth_utils.to_text, *args)
+@pytest.mark.parametrize("value", text_if_str_cases, ids=text_if_str_ids)
+def test_text_if_str(benchmark: BenchmarkFixture, value: Any) -> None:
+    benchmark(_batch, 10, eth_utils.text_if_str, eth_utils.to_text, value)
 
 @pytest.mark.benchmark(group="text_if_str")
-@pytest.mark.parametrize("args", text_if_str_cases, ids=text_if_str_ids)
-def test_faster_text_if_str(benchmark: BenchmarkFixture, args: Tuple[Any, ...]) -> None:
-    benchmark(_batch, 10, faster_eth_utils.text_if_str, faster_eth_utils.to_text, *args)
+@pytest.mark.parametrize("value", text_if_str_cases, ids=text_if_str_ids)
+def test_faster_text_if_str(benchmark: BenchmarkFixture, value: Any) -> None:
+    benchmark(_batch, 10, faster_eth_utils.text_if_str, faster_eth_utils.to_text, value)
 
 @pytest.mark.benchmark(group="hexstr_if_str")
-@pytest.mark.parametrize("args", hexstr_if_str_cases, ids=hexstr_if_str_ids)
-def test_hexstr_if_str(benchmark: BenchmarkFixture, args: Tuple[Any, ...]) -> None:
-    benchmark(_batch, 10, eth_utils.hexstr_if_str, eth_utils.to_hex, *args)
+@pytest.mark.parametrize("value", hexstr_if_str_cases, ids=hexstr_if_str_ids)
+def test_hexstr_if_str(benchmark: BenchmarkFixture, value: Any) -> None:
+    benchmark(_batch, 10, eth_utils.hexstr_if_str, eth_utils.to_hex, value)
 
 @pytest.mark.benchmark(group="hexstr_if_str")
-@pytest.mark.parametrize("args", hexstr_if_str_cases, ids=hexstr_if_str_ids)
-def test_faster_hexstr_if_str(benchmark: BenchmarkFixture, args: Tuple[Any, ...]) -> None:
-    benchmark(_batch, 10, faster_eth_utils.hexstr_if_str, faster_eth_utils.to_hex, *args)
+@pytest.mark.parametrize("value", hexstr_if_str_cases, ids=hexstr_if_str_ids)
+def test_faster_hexstr_if_str(benchmark: BenchmarkFixture, value: Any) -> None:
+    benchmark(_batch, 10, faster_eth_utils.hexstr_if_str, faster_eth_utils.to_hex, value)
