6263ce94079285c602ac01d5996f1d47ab5d24de
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-10-09 21:11:12 -0400

    Update applicators.py

--- faster_eth_utils/applicators.py
+++ faster_eth_utils/applicators.py
@@ -106,7 +106,7 @@ def apply_formatter_if(
 ) -> Union[TArg, TReturn]: ...
 
 def apply_formatter_if(
-    condition: Union[Callable[[TArg], TypeGuard[TOther]], Callable[[TArg], bool]],
+    condition: Union[Callable[[TArg], TypeGuard[TOther]], Callable[[Any], TypeGuard[TOther]], Callable[[TArg], bool]],
     formatter: Union[Callable[[TOther], TReturn], Callable[[TArg], TReturn]],
     value: TArg,
 ) -> Union[TArg, TReturn]:
