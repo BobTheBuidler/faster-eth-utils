614244649216a819fa5f648975575f36cb1d0f8e
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-10-19 14:18:22 -0400

    Update test_abi_utils.py

--- tests/core/abi-utils/test_abi_utils.py
+++ tests/core/abi-utils/test_abi_utils.py
@@ -779,9 +779,9 @@ def test_filter_abi_by_type_raises_for_invalid_abi_type() -> None:
     contract_abi = build_contract_abi([ABI_FUNCTION_TOKEN_LAUNCHED])
     with pytest.raises(
         ValueError,
-        match=re.escape("Unsupported ABI type: typing.Literal['notanabitype']"),  # type: ignore
+        match=re.escape("Unsupported ABI type: 'notanabitype'"),
     ):
-        filter_abi_by_type(Literal["notanabitype"], contract_abi)
+        filter_abi_by_type("notanabitype", contract_abi)
 
 
 @pytest.mark.parametrize(
