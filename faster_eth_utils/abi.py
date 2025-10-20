eeb9f3b8ea719e6e1b3b36986fda78f2873fddc8
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-10-20 04:04:21 -0400

    Update abi.py

--- faster_eth_utils/abi.py
+++ faster_eth_utils/abi.py
@@ -588,7 +588,7 @@ def get_aligned_abi_inputs(
         aligned_args = tuple(map(_align_abi_input, abi_element_inputs, normalized_args))
     elif isinstance(normalized_args, dict):
         # `args` is dict.  Align values according to abi order.
-        # NOTE While the 'else' clause would work for dict instances as well, dict instancecheck is fast,
+        # NOTE While the 'else' clause would work for dict instances as well, dict instancecheck is fast
         # and the subsequent code block compiles to faster C code when type is known to be dict
         aligned_args = tuple(
             map(
