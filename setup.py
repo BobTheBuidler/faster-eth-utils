1e75628e70b2a978631416342a67cefe92b05946
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-10-26 18:34:21 -0400

    fix(mypy): enable err code typeddict-item (#152)
    * fix(mypy): enable err code typeddict-item
    
    * chore: compile C files for source control
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -46,7 +46,6 @@ else:
             "--install-types",
             "--disable-error-code=attr-defined",
             "--disable-error-code=comparison-overlap",
-            "--disable-error-code=typeddict-item",
             "--disable-error-code=no-any-return",
             "--disable-error-code=misc",
             "--disable-error-code=unused-ignore",
